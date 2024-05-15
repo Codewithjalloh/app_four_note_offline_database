416334fa413c9957a1bddc063640396daf393ffb
codewithjalloh <codewithjalloh@gmail.com>
2024-05-15 20:27:20 +0100

    setting page update

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -1,4 +1,7 @@
+import 'package:app_four_note_offline_database/theme/theme_provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
@@ -17,11 +20,29 @@ class SettingPage extends StatelessWidget {
           color: Theme.of(context).colorScheme.primary,
           borderRadius: BorderRadius.circular(12),
         ),
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 25),
-        margin: EdgeInsets.only(left: 25, right: 25, top: 10),
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+        margin: const EdgeInsets.only(left: 25, right: 25, top: 10),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [],
+          children: [
+            // DARK MODE
+            Text(
+              "Dark Mode",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.inversePrimary),
+            ),
+
+            // SWITCH TOGGLE
+            // switch toggle
+            CupertinoSwitch(
+              value:
+                  Provider.of<ThemeProvider>(context, listen: false).isDarkMode,
+              onChanged: (value) =>
+                  Provider.of<ThemeProvider>(context, listen: false)
+                      .toggleTheme(),
+            ),
+          ],
         ),
       ),
     );
