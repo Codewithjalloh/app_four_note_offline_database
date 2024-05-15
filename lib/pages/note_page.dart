366986e3872a2292cfb097362b88c85869be68bf
codewithjalloh <codewithjalloh@gmail.com>
2024-05-15 19:00:33 +0100

    note page added

--- lib/pages/note_page.dart
+++ lib/pages/note_page.dart
@@ -1,4 +1,7 @@
+import 'package:app_four_note_offline_database/models/note.dart';
+import 'package:app_four_note_offline_database/models/note_database.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class NotePage extends StatefulWidget {
   const NotePage({super.key});
@@ -20,13 +23,67 @@ class _NotePageState extends State<NotePage> {
   }
 
   // CREATE NOTE
-  void createNote() {}
+  void createNote() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          MaterialButton(onPressed: () {
+            // add to db
+            context.read<NoteDatabase>().addNote(textController.text);
+
+            // clear controller
+            textController.clear();
+
+            // pop dialog box
+            Navigator.pop(context);
+          }),
+        ],
+      ),
+    );
+  }
 
   // READ NOTE
-  void readNote() {}
+  void readNote() {
+    context.read<NoteDatabase>().fetchNotes();
+  }
+
+  // UPDATE
+  void updateNote(Note note) {
+    textController.text = note.text;
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          MaterialButton(onPressed: () {
+            // add to db
+            context
+                .read<NoteDatabase>()
+                .updateNote(note.id, textController.text);
+
+            // clear controller
+            textController.clear();
+
+            // pop dialog box
+            Navigator.pop(context);
+          }),
+        ],
+      ),
+    );
+  }
 
   // DELETE NOTE
-  void deleteNote() {}
+  void deleteNote(int id) {
+    context.read<NoteDatabase>().deleteNote(id);
+  }
 
   @override
   Widget build(BuildContext context) {
