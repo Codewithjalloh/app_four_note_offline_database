1ff4baadef452f5c335c91694be1cd4e440594cc
codewithjalloh <codewithjalloh@gmail.com>
2024-05-16 23:41:27 +0100

    note_tile added

---
+++ lib/components/note_tile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NoteTile extends StatelessWidget {
+  final String text;
+
+  const NoteTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
